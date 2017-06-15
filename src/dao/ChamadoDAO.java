398613bd67ea49b00df07d7afc3e579e278a910f
LUIGI <luigi.inazawa.182@gmail.com>
2017-06-14 21:48:07 -0300

    ...

--- src/dao/ChamadoDAO.java
+++ src/dao/ChamadoDAO.java
@@ -2,11 +2,11 @@ package dao;
 
 import java.util.List;
 
-import model.Funcionario;
+import model.Chamado;
 
 public interface ChamadoDAO {
 	
-	public void adicionar(Chamado );
-	public List<Funcionario> pesquisarPorDocumento(String cpfFuncionario);
+	public void adicionar(Chamado c);
+	public List<Chamado> pesquisarPorDocumento(String cnpjEmpresa);
 
 }
